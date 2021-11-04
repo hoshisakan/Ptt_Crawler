ccec80c81cf6ef86551263c883657e0308ff74b7
YiQuan Zhao <hoshisakan@gmail.com>
2021-11-05 00:20:46 +0800

    2021-11-04-second-commit-modify-generate-executable-files-shell-script

--- generate.sh
+++ generate.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 set -e
 
-DIR="execute"
+# DIR="execute"
+DIR="execute_for_no_img"
 if [ -d "$DIR" ]; then
     echo "Will be remove the diretory"
-    rm -rf execute
+    rm -rf $DIR
     if [ $? -eq 0 ]; then
         echo "Remove the directory successfully!"
     else
@@ -12,4 +13,5 @@ if [ -d "$DIR" ]; then
         exit 1
     fi
 fi
-pyinstaller.exe --specpath ./execute/ --distpath ./execute/dist --workpath ./execute/build --add-data "D:\Files\Project\Ptt_Crawler\crawler.ini;." -D crawler_ptt.py
+# pyinstaller.exe --specpath ./execute/ --distpath ./execute/dist --workpath ./execute/build --add-data "D:\Files\Project\Ptt_Crawler\crawler.ini;." -D crawler_ptt.py
+pyinstaller.exe --specpath ./execute_for_no_img/ --distpath ./execute_for_no_img/dist --workpath ./execute_for_no_img/build --add-data "D:\Files\Project\Ptt_Crawler\no_img_crawler.ini;." -D crawler_ptt_no_image.py
